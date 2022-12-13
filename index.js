bc279db13d712e089c542cfa2163f21a0e364986
srjizhere <bhiapatil@gmail.com>
2022-12-14 04:52:03 +0530

    as

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-app = express()
+const app = express()
 app.use(cors())
+
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
